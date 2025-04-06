045e24116ac3e33f04b9d5af5d030db6f2d3e829
Yotam <yotamg6@gmail.com>
2025-04-06 16:26:21 +0300

    removed redundant file

--- src/lib/utils/general.ts
+++
@@ -1,6 +0,0 @@
-import { UploadStatus } from "@/types/video";
-import { MAX_UPLOAD_STATUS } from "./constants";
-
-export const getProgressValue = (status: UploadStatus): number => {
-  return (status / MAX_UPLOAD_STATUS) * 100;
-};
